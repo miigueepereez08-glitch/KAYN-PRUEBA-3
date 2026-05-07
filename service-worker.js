aa34f81d0650e1740f3582bca333b74e132b984c
miigueepereez08-glitch <miigueepereez08@gmail.com>
2026-05-07 07:14:47 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='kayn-v11-4-editor-modulos';
+const CACHE_NAME='kayn-v11-5-snap-magnetico';
 const ASSETS=['/','/index.html','/editor-modulos.html','/manifest.json','/logo.png','/icon-192.png','/icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
